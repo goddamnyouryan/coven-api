12c7404d0548b3538102f0f1002446b65c982f92
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-11-14 09:45:12 -0800

    Adding product hunt.

---
+++ lib/source/product_hunt.rb
@@ -0,0 +1,43 @@
+module Source
+  class ProductHunt < Base
+    attr_reader :token
+
+    def initialize
+      super
+      @token = post('oauth/token', token_options)['access_token']
+    end
+
+    def token_options
+      {
+        client_id: ENV['PRODUCT_HUNT_CLIENT'],
+        client_secret: ENV['PRODUCT_HUNT_SECRET'],
+        grant_type: 'client_credentials'
+      }
+    end
+
+    def feed_items
+      get('posts', {}, headers)['posts']
+    end
+
+    def headers
+      {
+        Accept: 'application/json',
+        'Content-Type' => 'application/json',
+        Authorization: "Bearer #{token}",
+      }
+    end
+
+    def attribute_map(item)
+      {
+        url: item['redirect_url'],
+        title: item['name'],
+        comments: item['discussion_url'],
+        external_created_at: DateTime.parse(item['created_at'])
+      }
+    end
+
+    def api
+      'https://api.producthunt.com/v1/'
+    end
+  end
+end
---
+++ spec/lib/source/product_hunt_spec.rb
@@ -0,0 +1,33 @@
+Dotenv.load
+require 'source/base'
+require 'source/product_hunt'
+require 'vcr'
+require 'support/vcr'
+
+describe Source::ProductHunt do
+
+  describe '.new' do
+    let(:object) { Source::ProductHunt.new }
+
+    it 'authorizes via oauth', :vcr do
+      object
+    end
+  end
+
+  describe '.feed_items' do
+    let(:object) { Source::ProductHunt.new }
+
+    it 'returns an array of feed items', :vcr do
+      items = object.translate
+
+      expect(items).to be_present
+
+      items.each do |item|
+        expect(item).to have_key(:url)
+        expect(item).to have_key(:title)
+        expect(item).to have_key(:comments)
+        expect(item).to have_key(:external_created_at)
+      end
+    end
+  end
+end
